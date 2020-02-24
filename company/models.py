37d51a75508f3c9db3a4722b64c042bb28fc014a
Alison <zhangxt0222@163.com>
2020-02-24 23:53:59 +0800

    user page frontend

--- company/models.py
+++ company/models.py
@@ -3,8 +3,3 @@ from django.core.validators import int_list_validator
 
 # Create your models here.
 
-class clients(models.Model):
-    username = models.CharField(max_length=100)
-    location = models.CharField(max_length=50, null=True, default=None)
-    timezone = models.DateTimeField(null=True, default=None)
-    email = models.EmailField(max_length=50)
--- user/models.py
+++ user/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.core.validators import int_list_validator
-
+from django.utils import timezone
 # Create your models here.
 
 class coders(models.Model):
@@ -18,11 +17,51 @@ class coders(models.Model):
     phone = models.CharField(max_length=50,null=True, default=None)
     intro = models.CharField(max_length=1000, null=True, default=None)
     # location and timezone will be left null so far
+
+class application(models.Model):
+    time = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=50, null=True, default=None)
+    coder = models.ForeignKey(to=coders, related_name="coder", null=True, blank=True, default=None,
+                                 on_delete=models.CASCADE)
+    company = models.ForeignKey(to=company, related_name="company", null=True, blank=True, default=None,
+                                 on_delete=models.CASCADE)
+    feedback = models.CharField(max_length=1000, null=True, default=None)
+ 
+
+
+class company(models.Model):
+    name = models.CharField(max_length=50, null=True, default=None)
+    industry = models.CharField(max_length=50, null=True, default=None)
+    location = models.CharField(max_length=50, null=True, default=None)
+    foundDate = models.DateTimeField(default=timezone.now)
+    companySize = models.CharField(max_length=50, null=True, default=None)
+    fundingType = models.CharField(max_length=50, null=True, default=None)
+    jobType = models.CharField(max_length=100, null=True, default=None)
+    intro = models.CharField(max_length=1000, null=True, default=None)
+
+    description = models.CharField(max_length=1000, null=True, default=None)
+    vision = models.CharField(max_length=1000, null=True, default=None)
+    companyVisit = models.CharField(max_length=1000, null=True, default=None)
+    vision = models.CharField(max_length=1000, null=True, default=None)
+    teamMeeting = models.CharField(max_length=1000, null=True, default=None)
+
+    email = models.EmailField(max_length=50,null=True, default=None)
+    twitter = models.URLField(max_length=250, null=True, default=None)
+    linkedin = models.URLField(max_length=250, null=True, default=None)
+    facebook = models.URLField(max_length=250, null=True, default=None)
+    ins = models.URLField(max_length=250, null=True, default=None)
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=50, null=True, default=None)
     
 
+
 class experienceInfo(models.Model):
-    name = models.CharField(max_length=50)
-    field = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=True, default=None)
+    field = models.CharField(max_length=50, null=True, default=None)
+    time = models.DateTimeField(default=timezone.now)
+    link = models.URLField(max_length=250, null=True, default=None)
     detail = models.CharField(max_length=1000, null=True, default=None)
     coder = models.ForeignKey(to=coders, related_name="experience", null=True, blank=True, default=None,
                                  on_delete=models.CASCADE)
@@ -30,6 +69,9 @@ class experienceInfo(models.Model):
 class projectInfo(models.Model):
     name = models.CharField(max_length=50)
     field = models.CharField(max_length=50)
+    time = models.DateTimeField(default=timezone.now)
+    github = models.URLField(max_length=250, null=True, default=None)
     detail = models.CharField(max_length=1000, null=True, default=None)
     coder = models.ForeignKey(to=coders, related_name="project", null=True, blank=True, default=None,
                                  on_delete=models.CASCADE)
+
